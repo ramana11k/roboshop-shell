75f805a7de19a8b3e28188d84855fd3942e9ba08
ramana11k <ramana11k@gmail.com>
2024-01-07 13:18:45 +0530

    delete old logs5

--- 10-delete-oldlogs.sh
+++ 10-delete-oldlogs.sh
@@ -13,7 +13,7 @@ fi
 
 FILE_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 
-while IFS=read -r line
+while IFS= read -r line
     do 
         echo -e "Deleting fiel: $line"
     done <<< $FILE_TO_DELETE
