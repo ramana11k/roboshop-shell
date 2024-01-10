35ab2b364d990702dea829e120d917397db96259
ramana11k <ramana11k@gmail.com>
2024-01-10 23:42:07 +0530

    auto creation of instances

--- roboshop.sh
+++ roboshop.sh
@@ -2,19 +2,21 @@
 
 AMI=ami-03265a0778a880afb
 SG_ID=sg-0b836ea10d115e212
-INSTANCES=("mongodb" "redit" "mysql" "rabbitmq" "catalogue" "user" "cart" "Shipping" "payment" "dispatch" "web")
+INSTANCES=("mongodb" "user" )
+
+#"redit" "mysql" "rabbitmq" "catalogue" "cart" "Shipping" "payment" "dispatch" "web"
 
 for i in ${INSTANCES[@]}
 
 do
-    echo "instance: $i"
-    if [ $i="mongobd"] ||[ $i="mysql"] || [ $i="shipping"]
+    echo "Instance: $i"
+    if [ $i="mongobd" ] || [ $i="mysql" ] || [ $i="shipping" ]
     then
         INSTANCE_TYPE="t3.small"
     else 
         INSTANCE_TYPE="t2.medium"
     fi
 
-    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-grop $SG_ID
+    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group $SG_ID
 
 done 
