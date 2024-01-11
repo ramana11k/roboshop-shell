c493d834cb7f285977903b7a6811cedc73e5acc2
ramana11k <ramana11k@gmail.com>
2024-01-11 23:38:28 +0530

    roboshop auto create instances --zone

--- roboshop.sh
+++ roboshop.sh
@@ -3,6 +3,8 @@
 AMI=ami-03265a0778a880afb
 SG_ID=sg-0b836ea10d115e212
 INSTANCES=("mongodb" "user")
+HOSTED_ZONE=Z0033627RS1P0QCI2QQR
+DOMAIN_NAME=nikhildevops.online
 
 #"redit" "mysql" "rabbitmq" "catalogue" "cart" "Shipping" "payment" "dispatch" "web"
 
@@ -23,4 +25,22 @@ do
 
     #aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t3.small --security-group-ids sg-0b836ea10d115e212 --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=Mongodb}]" --query 'Insstances[0].PrivateIpAddress' --output text
 
+    aws route53 change-resource-record-sets \
+        --hosted-zone-id $HOSTED_ZONE\
+        --change-batch '
+    {
+        "Comment": "Testing creating a record set"
+        ,"Changes": [{
+        "Action"              : "CREATE"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'" $i ".$DOMAIN_NAME"
+            ,"Type"             : "CNAME"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "$IP_ADDRESS"
+            }]
+        }
+        }]
+    }
+    '
 done 
