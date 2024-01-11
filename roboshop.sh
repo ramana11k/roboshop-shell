49dbaccc225721d8d2b8f7af4d654c2a43cddc8e
ramana11k <ramana11k@gmail.com>
2024-01-11 23:21:13 +0530

    auto creation of instances using script

--- roboshop.sh
+++ roboshop.sh
@@ -17,6 +17,10 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=$INSTANCES}]"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+
+    echo "$i : $IP_ADDRESS"
+
+    #aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t3.small --security-group-ids sg-0b836ea10d115e212 --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=Mongodb}]" --query 'Insstances[0].PrivateIpAddress' --output text
 
 done 
