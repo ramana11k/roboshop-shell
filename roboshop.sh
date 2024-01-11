1e66a85413e7d3e3b394d76518c61b5f0104521c
ramana11k <ramana11k@gmail.com>
2024-01-11 17:04:09 +0530

    roboshop auto create instances --name

--- roboshop.sh
+++ roboshop.sh
@@ -17,5 +17,6 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=$i}]"
+    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=$INSTANCES}]"
+
 done 
